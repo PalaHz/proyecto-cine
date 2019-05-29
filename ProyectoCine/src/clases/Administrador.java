023fc410e5e2cc5238f17e3540b9bc39cae6bed4
Pala <andrespala2000@gmail.com>
2019-05-28 22:56:24 -0500

    Admin y Vender

--- ProyectoCine/src/clases/Administrador.java
+++ ProyectoCine/src/clases/Administrador.java
@@ -5,42 +5,20 @@ import java.util.*;
 public class Administrador extends Persona {
     public String user;
     public String password;
-    public String cargo;
-    public Cartelera registra;
     public Administrador() {    
     }
 
     public Administrador(String user, String password, String cargo, Cartelera registra) {
         this.user = user;
         this.password = password;
-        this.cargo = cargo;
-        this.registra = registra;
     }
 
     public Administrador(String user, String password, String cargo, Cartelera registra, String nombres, String apellidos, String cedula) {
         super(nombres, apellidos, cedula);
         this.user = user;
         this.password = password;
-        this.cargo = cargo;
-        this.registra = registra;
     }
 
-    
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public Cartelera getRegistra() {
-        return registra;
-    }
-
-    public void setRegistra(Cartelera registra) {
-        this.registra = registra;
-    }
 
     public String getNombres() {
         return nombres;
